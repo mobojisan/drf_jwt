365ae94f87b74f0cc42d4149344d8c9df8476254
mobojisan <r.kasubuchi@xmart.co.jp>
2022-09-06 11:48:59 +0900

    add a filter test

--- apiv1/tests.py
+++ apiv1/tests.py
@@ -45,11 +45,41 @@ class TestHoge(TestCase):
             # 取得したトークンを設定
             HTTP_AUTHORIZATION=f"Bearer {refresh.json().get('access')}",
         )
+        # ここに
+        # print(response.json())
         # pretty-print
         # print(json.dumps(response.json(), indent=4))
         # print(result.content)
         # print(response.status_code)
         self.assertEqual(response.status_code, 200)
         
-        def test_filter(self):
-            pass
+    def test_filter(self):
+        refresh = Client().post(
+            '/api/v1/auth/jwt/create/',
+            data={
+                'username': 'root',
+                'password': 'toor',
+            }
+        )
+        import json
+        # print(json.dumps(refresh.json(), indent=4))
+        # print(refresh.__dict__)
+        response = Client().get(
+            '/api/v1/books/',
+            {
+                'price__gte': 1000
+            },
+            content_type='application/json',
+            # 取得したトークンを設定
+            HTTP_AUTHORIZATION=f"Bearer {refresh.json().get('access')}",
+        )
+        self.assertEqual(response.json(), { 
+            'count': 2,
+            'next': None, 
+            'previous': None, 
+            'results': [
+                    {'id': '6e3d5daf-ad3c-40f9-a981-eb4545c1ce33', 'title': 'DRFの教科書', 'price': 2000},
+                    {'id': '7334efe9-96f5-4697-b479-55673f9aa098', 'title': 'DRFの教科書', 'price': 2000}
+                ]
+            }
+        )
